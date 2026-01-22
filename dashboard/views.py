9ef1814af6313afd850b89dc38d9b3e01dc4462e
valu28d <valentinafalconi28@gmail.com>
2026-01-21 20:53:04 -0500

    Guia 26 de nuevo debido a errores

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 import requests
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-    
+from django.contrib.auth.decorators import login_required, permission_required
+   
     
 @login_required
+@permission_required('dashboard.index_viewer', raise_exception=True)
 def dashboard(request):
 
     response = requests.get(settings.API_URL)  # URL de la API
